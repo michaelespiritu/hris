d9123d55151cbf2481a2031815ceb329fd6779ba
michaelespiritu <michaelgeraldespiritu@gmail.com>
2015-07-25 04:32:09 +0800

    search updated

--- templates/dashboard/search/search.php
+++ templates/dashboard/search/search.php
@@ -4,15 +4,18 @@
   <div class="col-md-12 padding-15">
     <h3>Employee Search:</h3>
     <div class="form-group">
-      <select class="form-control" onchange="showUser(this.value)">
-        <option></option>
-          <?php foreach ($employee as $info ) {
-            echo '<option value="'.$info->user_id.'">'.$info->last_name.', '.$info->first_name.'</option>';
-          } ?>
-          </select>
-      </div>
-      <div id="txtHint">
-
+      <?php if(count($employee)): ?>
+        <select class="form-control" onchange="showUser(this.value)">
+          <option></option>
+            <?php foreach ($employee as $info ) {
+              echo '<option value="'.$info->user_id.'">'.$info->last_name.', '.$info->first_name.'</option>';
+            } ?>
+            </select>
+        </div>
+        <div id="txtHint">
+      <?php else: ?>
+        <h3 class="text-center">No Registered Employees.</h3>
+      <?php endif; ?>
       </div>
   </div>
 </div>
